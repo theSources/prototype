04a5467da701b33910df93f051a529110dcf8226
Mike Limansky <mike.limansky@gmail.com>
2015-02-14 02:25:31 +0300

    Start webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require("gulp");
-var bower = require("gulp-bower");
+var gulp = require('gulp');
+var bower = require('gulp-bower');
+var webserver = require('gulp-webserver');
 
 var config = {
     bowerDir : './bower_components'
@@ -7,6 +8,13 @@ var config = {
 
 gulp.task('bower', function() {
     return bower().pipe(gulp.dest(config.bowerDir));
+});
+
+gulp.task('server', function() {
+    gulp.src('html')
+        .pipe(webserver({
+            livereload: true
+        }));
 });
 
 gulp.task('default', ['bower'], function() {
