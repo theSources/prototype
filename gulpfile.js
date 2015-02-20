7de863dc8f8f7ae17abddc94a6d78d3b22710607
Victor Portnov <vic.portnov@gmail.com>
2015-02-21 01:20:28 +0300

    Add watch; split 'clean' and 'build' to enable live reload on watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower-deps');
 var webserver = require('gulp-webserver');
+var cached = require('gulp-cached');
+var remember = require('gulp-remember');
 
 var del = require('del');
 
@@ -20,23 +22,48 @@ var config = {
   }
 };
 
-
 bower = bower(config.bower);
-
 bower.installtask(gulp);
 
+
+var paths = {
+  dist: './dist',
+  html: './html'
+};
+
+var allFiles = function (path) {
+  return path + '/*';
+};
+
+var sources = [allFiles(paths.html)].concat(bower.deps);
+
+
 gulp.task('server', function() {
-  gulp.src('./dist')
+  gulp.src(paths.dist)
       .pipe(webserver({
         livereload: true
       }));
 });
 
-gulp.task('build', ['bower'], function () {
-  del('./dist');
+gulp.task('clean', function () {
+  del(paths.dist);
+});
 
-  return gulp.src(['./html/*'].concat(bower.deps))
-             .pipe(gulp.dest('./dist'));
+gulp.task('build', ['bower'], function () {
+  return gulp.src(sources)
+             .pipe(cached('build'))
+             .pipe(remember('build'))
+             .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('watch', function () {
+  var watcher = gulp.watch(sources, ['build']);
+  watcher.on('change', function (event) {
+    if (event.type === 'deleted') {
+      delete cached.caches.build[event.path];
+      remember.forget('build', event.path);
+    }
+  });
 });
 
 
